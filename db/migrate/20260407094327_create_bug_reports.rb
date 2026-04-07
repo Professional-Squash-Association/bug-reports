65126e42ff0ccbcee40a3dc2c4305f9f25d9719e
gary yau <gary.yau@hotmail.com>
2026-04-07 11:07:27 +0100

    refactor: App more fields into migration

--- db/migrate/20260407094327_create_bug_reports.rb
+++ db/migrate/20260407094327_create_bug_reports.rb
@@ -1,19 +1,30 @@
 class CreateBugReports < ActiveRecord::Migration[8.1]
   def change
     create_table :bug_reports do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :description
       t.text :steps_to_reproduce
-      t.string :severity
-      t.string :source
-      t.string :reporter_email
+      t.string :severity, default: "medium"
+      t.string :source, null: false
+      t.string :reporter_email, null: false
       t.string :reporter_name
-      t.string :status
+      t.string :status, default: "pending"
+      t.string :image_url
+      t.string :callback_url
       t.string :github_issue_url
       t.integer :github_issue_number
       t.string :github_repo
 
       t.timestamps
     end
+
+    create_table :api_keys do |t|
+      t.string :token, null: false
+      t.string :name, null: false
+
+      t.timestamps
+    end
+
+    add_index :api_keys, :token, unique: true
   end
 end
