18eadb27275a7728f2bcb8e2def664e1e0735bb6
gary yau <gary.yau@hotmail.com>
2026-04-07 10:47:05 +0100

    chore: run migrations

---
+++ db/schema.rb
@@ -0,0 +1,29 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.1].define(version: 2026_04_07_094327) do
+  create_table "bug_reports", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.integer "github_issue_number"
+    t.string "github_issue_url"
+    t.string "github_repo"
+    t.string "reporter_email"
+    t.string "reporter_name"
+    t.string "severity"
+    t.string "source"
+    t.string "status"
+    t.text "steps_to_reproduce"
+    t.string "title"
+    t.datetime "updated_at", null: false
+  end
+end
